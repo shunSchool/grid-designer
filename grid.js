c0679d51d8daa2ea46c1b5f39733031d9b3b22da
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-12-07 11:27:50 -0700

    color and movement

--- grid.js
+++ grid.js
@@ -5,12 +5,12 @@ function createGridArray() {
     return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 0, 1, 0],
-            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-            [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 }
 
@@ -19,11 +19,46 @@ function createDivGrid() {
         for (let col = 0; col < NUM_COLS; col++) {
           // create a div for each element in 2d grid
           let divEl = document.createElement("div");
+
+            //add an id to each divEl
+            divEl.id = "cell" + row + "-" + col;000
+
+          //add appropriate class to each divEl
           if (grid[row][col] == 1) {
               divEl.classList.add("grey");
+          } else if (grid[row][col] == 2) {
+              divEl.classList.add("player");
           }
+
+          //add dataset values for row and col
+          divEl.dataset.row = row;
+          divEl.dataset.col = col;
+          
+          //add event lsitener to each div element
+          divEl. addEventListener("click", cellClicked)
+
           //add div to container
           document.getElementById("container").append(divEl);
         }
+    }
+}
+
+function cellClicked(event) {
+    // set  the color of the clicked cell
+
+    //get value of select color element
+    let color = document.getElementById("cell-color").value;
+
+    //get row and col of the clicked cell
+    let row = event.target.dataset.row;
+    let col = event.target.dataset.col;
+
+
+    //update clicked cell based on selected color
+    event.target.classList = ""; //empty cell class list
+    grid[row][col] = 0; //set grid to white
+    if (color == "grey") {
+        event.target.classList.add("grey")
+        grid[row][col] = 1;
     }
 }
--- main.js
+++ main.js
@@ -8,5 +8,46 @@ const NUM_COLS = 10;
 //create an array to represent a grid
 let grid = createGridArray();
 
+//add a player to the grid array
+let player = {
+    row: 5,
+    col: 5
+}
+
+grid[player.row][player.col] = 2;
+
 // create divs to model grid array
 createDivGrid();
+
+//player movement - key event listeners
+document.addEventListener("keydown", movePlayer);
+
+function movePlayer(event) {
+    if(event.keyCode == 39) {//right arrow key
+        updatePlayer(player.row, player.col + 1);
+    }else if (event.keyCode == 37) { //left arrow key
+        updatePlayer(player.row, player.col - 1);
+    }else if (event.keyCode == 38) { //up arrow key
+        updatePlayer(player.row - 1, player.col);
+    }else if (event.keyCode == 40) { //down arrow key
+        updatePlayer(player.row + 1, player.col);
+    }
+}
+
+function updatePlayer(newRow, newCol) {
+     //remove player class from current location
+     let cellId = "cell" + player.row + "-" + player.col
+     document.getElementById(cellId).classList.remove("player");
+ 
+     //Set grid array to 0 for current location
+     grid[player.row][player.col] = 0;
+ 
+     //update player location
+     player.row = newRow;
+     player.col = newCol;
+ 
+     //update class and grid
+     cellId = "cell" + player.row + "-" + player.col
+     document.getElementById(cellId).classList.add("player");
+     grid[player.row][player.col] = 2;
+}
