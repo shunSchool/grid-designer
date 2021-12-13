f8954940b38dc4c95f444ad51119389f29104c79
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-12-13 10:20:07 -0700

    boundaries

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ let grid = createGridArray();
 
 //add a player to the grid array
 let player = {
-    row: 5,
-    col: 5
+    row: 0,
+    col: 3
 }
 
 grid[player.row][player.col] = 2;
@@ -23,31 +23,31 @@ createDivGrid();
 document.addEventListener("keydown", movePlayer);
 
 function movePlayer(event) {
-    if(event.keyCode == 39) {//right arrow key
+    if (event.keyCode == 39 && player.col < 9 && (cell-color (0))) {//right arrow key
         updatePlayer(player.row, player.col + 1);
-    }else if (event.keyCode == 37) { //left arrow key
+    }else if (event.keyCode == 37 && player.col > 0) { //left arrow key
         updatePlayer(player.row, player.col - 1);
-    }else if (event.keyCode == 38) { //up arrow key
+    } else if (event.keyCode == 38 && player.row > 0) { //up arrow key
         updatePlayer(player.row - 1, player.col);
-    }else if (event.keyCode == 40) { //down arrow key
+    } else if (event.keyCode == 40 && player.row < 9) { //down arrow key
         updatePlayer(player.row + 1, player.col);
     }
 }
 
 function updatePlayer(newRow, newCol) {
-     //remove player class from current location
-     let cellId = "cell" + player.row + "-" + player.col
-     document.getElementById(cellId).classList.remove("player");
- 
-     //Set grid array to 0 for current location
-     grid[player.row][player.col] = 0;
- 
-     //update player location
-     player.row = newRow;
-     player.col = newCol;
- 
-     //update class and grid
-     cellId = "cell" + player.row + "-" + player.col
-     document.getElementById(cellId).classList.add("player");
-     grid[player.row][player.col] = 2;
+    //remove player class from current location
+    let cellId = "cell" + player.row + "-" + player.col
+    document.getElementById(cellId).classList.remove("player");
+
+    //Set grid array to 0 for current location
+    grid[player.row][player.col] = 0;
+
+    //update player location
+    player.row = newRow;
+    player.col = newCol;
+
+    //update class and grid
+    cellId = "cell" + player.row + "-" + player.col
+    document.getElementById(cellId).classList.add("player");
+    grid[player.row][player.col] = 2;
 }
